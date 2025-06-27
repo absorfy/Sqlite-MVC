7d0ca64ae168b1689daf89251a2c2de89fad922a
Vladyslav Krykun <mr.laggermpc.890@gmail.com>
2025-06-27 14:54:49 +0200

    deleted unimportant files

--- MyMVCApp/Database/DbInitializer.cs
+++ MyMVCApp/Database/DbInitializer.cs
@@ -7,7 +7,6 @@ public static class DbInitializer
 {
     public static void Seed(SqlLiteDbContext context)
     {
-        SeedCars(context);
         SeedHeros(context);
     }
 
@@ -17,33 +16,6 @@ public static class DbInitializer
             return;
         
         context.Classes.AddRange(new MageClass(), new WarriorClass());
-        context.SaveChanges();
-    }
-
-    public static void SeedCars(SqlLiteDbContext context)
-    {
-        if(context.Movies.Any() || context.Genres.Any() || context.Sessions.Any())
-            return;
-        
-        var movie = new MovieEntity
-        {
-            Title = "The Matrix",
-            Director = "Wachowskis",
-            Description = "A computer hacker learns the true nature of reality.",
-            CreatedAt = DateTime.Now,
-            Genres = new List<GenreEntity>
-            {
-                new GenreEntity { Name = "Action", CreatedAt = DateTime.Now },
-                new GenreEntity { Name = "Sci-Fi", CreatedAt = DateTime.Now }
-            },
-            Sessions = new List<SessionEntity>
-            {
-                new SessionEntity { SessionDate = DateTime.Today.AddDays(1), CreatedAt = DateTime.Now },
-                new SessionEntity { SessionDate = DateTime.Today.AddDays(2), CreatedAt = DateTime.Now }
-            }
-        };
-
-        context.Movies.Add(movie);
         context.SaveChanges();
     }
 }
--- MyMVCApp/Database/SqlLiteDbContext.cs
+++ MyMVCApp/Database/SqlLiteDbContext.cs
@@ -6,11 +6,6 @@ namespace MyMVCApp.Database;
 
 public class SqlLiteDbContext : DbContext
 {
-    public DbSet<MovieEntity> Movies { get; set; }
-    public DbSet<GenreEntity> Genres { get; set; }
-    public DbSet<SessionEntity> Sessions { get; set; }
-    public DbSet<CarEntity> Cars { get; set; }
-    
     public DbSet<HeroEntity> Heroes { get; set; }
     public DbSet<AbstractClassEntity> Classes { get; set; }
 
@@ -21,16 +16,6 @@ public class SqlLiteDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<GenreEntity>()
-            .HasOne(g => g.Movie)
-            .WithMany(g => g.Genres)
-            .HasForeignKey(g => g.MovieId);
-        
-        modelBuilder.Entity<SessionEntity>()
-            .HasOne(s => s.Movie)
-            .WithMany(s => s.Sessions)
-            .HasForeignKey(s => s.MovieId);
-        
         modelBuilder.Entity<AbstractClassEntity>()
             .HasDiscriminator<string>("ClassType")
             .HasValue<WarriorClass>("Warrior")
